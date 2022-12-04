378c0505bf46cfc5cfafd2a5a6a792b77bc744a8
CyberWeel <cyberweel.work@gmail.com>
2022-12-04 19:23:38 +0500

    plugin empty initialization

---
+++ wp-content/plugins/cw-clients-blog/cw-clients-blog.php
@@ -0,0 +1,6 @@
+/**
+* Plugin Name: CyberWeel Clients Blog
+* Description: Plugin for blog, which can be filled with user's posts. They have to fill a form for it.
+* Version: 1.0.0
+* Author: CyberWeel
+*/
