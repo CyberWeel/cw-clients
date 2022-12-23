d8b0f96e6bc8eb6809bd7d94cba609752a184f48
CyberWeel <cyberweel.work@gmail.com>
2022-12-23 16:51:55 +0500

    New website

--- wp-content/plugins/cw-clients/core/shortcodes.php
+++
@@ -1,4 +0,0 @@
-<?php # A set of shortcodes for plugin
-# TODO: Add some examination for existing shortcodes. shortcode_exists()
-add_shortcode('cw_clients', array('CwClients', 'showClientsTemplate'));
-add_shortcode('cw_clients_form', array('CwClients', 'showFormTemplate'));
